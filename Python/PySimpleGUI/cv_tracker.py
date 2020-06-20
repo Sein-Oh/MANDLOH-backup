9def7e7cae3f3ef6849089287345628309d9fd98
Sein-Oh <48703174+Sein-Oh@users.noreply.github.com>
2020-06-20 23:23:26 +0900

    Create cv_tracker.py

---
+++ Python/PySimpleGUI/cv_tracker.py
@@ -0,0 +1,56 @@
+import cv2, PySimpleGUI as sg
+import tkinter
+w, h = 640, 480
+
+layout = [[sg.Graph((w,h),(0,h), (w,0), key='-GRAPH-', enable_events=True, drag_submits=True)],]
+window = sg.Window('Demo', layout, location=(10, 10), return_keyboard_events=True)
+fig = None
+mouse_down = False
+tracking = False
+cap = cv2.VideoCapture(0)
+cap.set(cv2.CAP_PROP_FRAME_WIDTH, w/2) #Set caputre size
+cap.set(cv2.CAP_PROP_FRAME_HEIGHT, h/2)
+
+
+while True:
+    ret, frame = cap.read()
+    frame = cv2.resize(frame, dsize=(w, h), interpolation=cv2.INTER_AREA)
+    event, values = window.read(timeout=0)
+    if event in ('Exit', None):
+        break
+
+    elif event == '-GRAPH-': #Mouse down event.
+        if not mouse_down: #Triggerd 1 time.
+            mouse_start = (values['-GRAPH-'][0], values['-GRAPH-'][1])
+            print('start:{}'.format(mouse_start))
+        mouse_pos = (values['-GRAPH-'][0], values['-GRAPH-'][1])
+        mouse_down = True
+        tracking = False
+
+    elif event == '-GRAPH-+UP': #Mouse up event.
+        mouse_down = False
+        if not tracking:
+            tracking = True
+            tracker = cv2.TrackerKCF_create()
+            bbox = (mouse_start[0], mouse_start[1], mouse_pos[0] - mouse_start[0], mouse_pos[1] - mouse_start[1])
+            tracker.init(frame, bbox)
+
+    elif event == 'r': #keyboard event
+        tracking = False
+        print('Release tracking mode.')
+
+    if tracking:
+        found, track_pos = tracker.update(frame)
+        if found:
+            p1 = (int(track_pos[0]), int(track_pos[1]))
+            p2 = (int(track_pos[0] + track_pos[2]), int(track_pos[1] + track_pos[3]))
+            cv2.rectangle(frame, p1, p2, (255,0,0), 2)
+
+    if mouse_down:
+        cv2.rectangle(frame, mouse_start, mouse_pos, (0,255,0), 2)
+
+    imgbytes = cv2.imencode('.png', frame)[1].tobytes()
+    if fig:
+        window['-GRAPH-'].delete_figure(fig)
+    fig = window['-GRAPH-'].draw_image(data=imgbytes, location=(0,0))
+    window['-GRAPH-'].TKCanvas.tag_lower(fig)
